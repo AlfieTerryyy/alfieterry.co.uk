4e1429d222faaa9bb05acf159764ecab41c3d3c0
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2024-07-08 22:38:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     if (window.scrollY > 50) {
-        header.style.backgroundColor = '#002244';
+        header.style.backgroundColor = '#444';
     } else {
-        header.style.backgroundColor = '#003366';
+        header.style.backgroundColor = '#333';
     }
 });
 
@@ -35,8 +35,8 @@ window.addEventListener('scroll', function () {
 window.addEventListener('scroll', function () {
     const footer = document.querySelector('footer');
     if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
-        footer.style.backgroundColor = '#002244';
+        footer.style.backgroundColor = '#444';
     } else {
-        footer.style.backgroundColor = '#003366';
+        footer.style.backgroundColor = '#333';
     }
 });
