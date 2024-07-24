f326c68640bbe9ca6e2e294ec5fbcd5bb642fc11
AlfieTerryyy <168709104+AlfieTerryyy@users.noreply.github.com>
2024-07-24 07:22:44 +0100

    Delete tags.js

--- tags.js
+++
@@ -1,8 +0,0 @@
-      document.addEventListener('DOMContentLoaded', function() {
-        fetch('tags.html')
-          .then(response => response.text())
-          .then(data => {
-            document.getElementById('tags').innerHTML = data;
-          })
-          .catch(error => console.error('Error loading tags:', error));
-      });
